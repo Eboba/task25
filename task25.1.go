63c7b7798ef069374e1aabe789524a8436a7c559
Eboba <opex99@mail.ru>
2022-03-13 19:39:24 +0300

    1

---
+++ task25.1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+func parseTest(text string, search string) bool {
+
+	if strings.Contains(text, search) { // данная фукция отлично работает с строками UTF-8
+		return true
+	} else {
+		return false
+	}
+}
+func main() {
+
+	fmt.Println("25.8 Практическая работа")
+	fmt.Println("")
+	fmt.Println("Задание 1. Программа для нахождения подстроки в кириллической подстроке.")
+	fmt.Println("------------")
+
+	var (
+		text   string
+		search string
+	)
+
+	flag.StringVar(&text, "str", "", "Enter text")
+	flag.StringVar(&search, "substr", "", "Search query")
+
+	flag.Parse()
+	fmt.Println(text, search)
+	fmt.Println(parseTest(text, search))
+}
